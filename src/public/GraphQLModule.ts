b3c68a0fb43d812b9e5f1b3140f8b0290f778220
Leonard Parisi <email@leonardparisi.com>
2020-12-17 12:13:09 -0500

    Added comments

--- src/public/GraphQLModule.ts
+++ src/public/GraphQLModule.ts
@@ -3,19 +3,37 @@ import { GraphQLSchemaModule } from 'apollo-server';
 import { ApolloServer } from 'apollo-server-express';
 import fs from 'fs';
 
+/**
+ * An EasyExpress module that adds an ApolloServer to one's express application.
+ */
 export class GraphQLModule implements IEasyExpressAttachableModule {
   private pathToModules: string;
   private server: ApolloServer | null;
 
+  /**
+   * Constructs a GraphQL module that will read graphql modules from the directory at the given path.
+   *
+   * @param pathToModules a path to the directory holding all graphql modules
+   */
   constructor(pathToModules: string) {
     this.pathToModules = pathToModules;
     this.server = null;
   }
 
-  attachTo(server: EasyExpressServer): Promise<unknown> {
+  /**
+   * Creates a new ApolloServer using the modules inside the directory at the given file path.
+   * Then, it attaches the /graphql endpoint to the app.
+   *
+   * @param server the EasyExpressServer that this module will be attached to
+   */
+  public attachTo(server: EasyExpressServer): Promise<unknown> {
     return this.startServer(server);
   }
 
+  /**
+   *
+   * @param server the EasyExpressServer that this module will be attached to
+   */
   private async startServer(server: EasyExpressServer) {
     const modules: GraphQLSchemaModule[] = await this.loadFiles<GraphQLSchemaModule>(this.pathToModules);
     this.server = new ApolloServer({ modules });
@@ -23,6 +41,12 @@ export class GraphQLModule implements IEasyExpressAttachableModule {
     console.log('🚀 Apollo Server listening at /graphql');
   }
 
+  /**
+   * Loads all files inside the directory at the given path and returns
+   * the contents as a list of <T>.
+   *
+   * @param path path to a directory holding files of type T
+   */
   private async loadFiles<T>(path: string): Promise<T[]> {
     return new Promise((resolve, reject) => {
       fs.readdir(path, async (err, filenames) => {
